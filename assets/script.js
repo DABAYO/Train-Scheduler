d3880f948255ec03531a38fe75b31d020a1d75e0
DABAYO <dbayo2000@yahoo.com>
2018-01-21 08:56:16 -0500

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var config = {
+    apiKey: "AIzaSyCBZ9CFHuQEbzYzOVKmnu4WNIa6sItykqg",
+    authDomain: "first-firebase-project-b83f1.firebaseapp.com",
+    databaseURL: "https://first-firebase-project-b83f1.firebaseio.com",
+    projectId: "first-firebase-project-b83f1",
+    storageBucket: "first-firebase-project-b83f1.appspot.com",
+    messagingSenderId: "700854584942"
+  };
+  firebase.initializeApp(config);
+
+  // variables
+  var database = firebase.database().ref();
+
+  
